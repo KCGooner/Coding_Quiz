248f2556f9671ed7a759e5b550b414a388f7636f
Joe Huelsman <kcgooner91@gmail.com>
2019-12-28 11:07:16 -0600

    added style sheet and arrays

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Inside which HTML element do we put the Javascript",
+      choices: ["<js>", "<script>", "<scripting>", "<javascript>"],
+      answer: "<script>"
+    },
+    {
+      title: "How do you create a function in Javascript?",
+      choices: ["function = myfunction()", "function:myfunction()", "function myfunction()"],
+      answer: "function myfunction()"
+    },
+    {
+      title: "How does a WHILE loop start?",
+      choices: ["while i=1 to 10","while (i <= 10; i++)", "while (i <=10)"],
+      answer: "while (i <= 10)"
+    }
+  ];
+  

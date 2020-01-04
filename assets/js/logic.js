55352a2a0afd6fc69e566ad0c4a2ecd113f32eea
Joe Huelsman <kcgooner91@gmail.com>
2020-01-03 20:53:20 -0600

    added js files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,21 @@
 var startBtn = document.getElementById('start');
 
 var submitBtn = document.getElementById('Submit');
+
+var currentQuestion = 0;
+
+startBtn.addEventListener('click', startQuiz);
+
+function quizQuestion(title, choices, answer) {
+    this.title = title;
+    this.choices = choices;
+    this.answer = answer;
+    console.log();
+}
+
+function startQuiz(questions) {
+    
+};
+
+
+

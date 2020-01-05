fda3950c233a424c3c1fd98d20fdc4cbfb08b896
Joe Huelsman <kcgooner91@gmail.com>
2020-01-05 15:36:55 -0600

    added javascript code for buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,34 +2,52 @@ var startBtn = document.getElementById('start');
 
 var submitBtn = document.getElementById('Submit');
 
-var currentQuestion = 0;
 var questionsNo = 0;
 
 //click to start quiz
 startBtn.addEventListener('click', startQuiz);
 startBtn.addEventListener('click', startTimer);
 
-
 function startQuiz() {
     displayQuiz(questionsNo);
-}
+};
+
 //generate questions
 function displayQuiz(questionsNo) {
-    console.log(questions[questionsNo]);
+
+    //empty out quiz intro
+    $("#start").click(function () {
+        $("#quizIntro").empty();
+    });
+
     questions[questionsNo].title, questions[questionsNo].choices;
 
     var questionTitle = document.createElement("div")
     questionTitle.innerHTML = questions[questionsNo].title
     document.getElementById("quizContainer").appendChild(questionTitle);
-
+    var title = questions[questionsNo].title
     var choices = questions[questionsNo].choices
-    for (var i = 0; i < choices.length; i++) {
 
-        var answerTitle = document.createElement("div")
+    for (var i = 0; i < choices.length; i++) {
+        var answerTitle = document.createElement("button")
         answerTitle.innerHTML = choices[i]
         document.getElementById("quizContainer").appendChild(answerTitle);
     };
-    questionsNo++;
+
+    //creates next question button
+    var nextQuestion = document.createElement("button");
+    nextQuestion.innerHTML = " Next Question ";
+    document.body.appendChild(nextQuestion);
+
+    // questionsNo++;
+
+    // eventListener to move to next question
+    // if/else for correct answer to questions
+    // if() {
+
+    // } else () {
+
+    // };
 
 };
 
@@ -37,10 +55,11 @@ function displayQuiz(questionsNo) {
 function startTimer() {
     var seconds = 70, $seconds = document.querySelector('#countdown');
     (function countdown() {
-        $seconds.textContent = "time left: " + seconds + (seconds == 1 ? '' : 's')
+        $seconds.textContent = "Time Left: " + seconds + (seconds == 1 ? '' : 's')
         if (seconds-- > 0) setTimeout(countdown, 750)
     })();
 };
+
 
 
 

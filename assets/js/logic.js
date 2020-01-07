e2fe0571e59c57d0730cc7183603dd8169f65f7a
Joe Huelsman <kcgooner91@gmail.com>
2020-01-07 07:50:38 -0600

    fixed a timer bug

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,8 @@
-
 var startBtn = document.getElementById('start');
 
 var submitBtn = document.getElementById('Submit');
 
 var answerStorage = [];
-
-var seconds = 70;
 
 // var questionsNo = 0;
 
@@ -24,15 +21,30 @@ function startQuiz() {
 function displayQuiz(questionsNo) {
     console.log(questionsNo);
 
-    // TODO: if no question end game
+   
     if (questions.length === questionsNo) {
         stop();
-    }
+
+
+        //creates submit button 
+        var submitButton = document.createElement("button")
+
+      
+
+        submitButton.innerHTML = "Submit Results"
+        document.getElementById('quizContainer').appendChild(submitButton);
+        submitButton.style.backgroundColor = "green"
+        submitButton.style.height = "150px"
+        submitButton.style.width = "250px"
+    };
 
 
     var questionTitle = document.createElement("div")
     questionTitle.innerHTML = questions[questionsNo].title
     document.getElementById("quizContainer").appendChild(questionTitle);
+
+    questionTitle.style.fontSize = "30px"
+
     var answer = questions[questionsNo].answer
     var choices = questions[questionsNo].choices
 
@@ -40,6 +52,12 @@ function displayQuiz(questionsNo) {
         var answerTitle = document.createElement("button")
         answerTitle.innerHTML = choices[i]
         answerTitle.value = choices[i]
+
+        answerTitle.style.backgroundColor = "gray"
+        answerTitle.style.height = "50px"
+        answerTitle.style.width = "150px"
+        answerTitle.style.margin = "10px"
+
         document.getElementById("quizContainer").appendChild(answerTitle);
 
         answerTitle.addEventListener("click", function (e) {
@@ -47,22 +65,28 @@ function displayQuiz(questionsNo) {
             console.log(e.target.value)
             console.log(answer)
             if (e.target.value === answer) {
+
+                // clear div
                 document.getElementById("quizContainer").innerHTML = "";
+
                 displayQuiz(++questionsNo);
-            }
+            };
         });
     };
-
 };
 
-//timer
+
+// timer
 function startTimer() {
-    $seconds = document.querySelector('#countdown');
+     $seconds = document.querySelector('#countdown');
     (function countdown() {
         $seconds.textContent = "Time Left: " + seconds + (seconds == 1 ? '' : 's')
         if (seconds-- > 0) setTimeout(countdown, 750)
     })();
 };
+
+
+var seconds = 75;
 
 
 
